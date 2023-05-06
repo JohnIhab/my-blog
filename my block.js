701d548843a3261312653f89b84d57893244a421
john <74468815+john-ihab@users.noreply.github.com>
2023-05-07 00:32:10 +0300

    my blog

---
+++ my block.js
@@ -0,0 +1,28 @@
+function clickfacebook() {
+    window.open("https://www.facebook.com/2002johncena", "_blank");
+}
+function clickwhats() {
+    window.open("https://web.whatsapp.com/", "_blank");
+}
+function clickgithub() {
+    window.open("https://github.com/john-ihab", "_blank");
+}
+function clickinstagram() {
+    window.open("https://www.instagram.com/official_johnihab/", "_blank");
+}
+function clicklinkedin() {
+    window.open("https://www.linkedin.com/in/john-ihab-58b74b204/", "_blank");
+}
+function clickyoutube() {
+    window.open("https://www.youtube.com/channel/UCRL2NgNk28JWIbRwggd_PBQ", "_blank");
+}
+function clicktele() {
+    window.open("https://web.telegram.org/k/", "_blank");
+}
+
+function next() {
+    window.location = "index.html";
+}
+function prev() {
+    window.location = "My blog.html";
+}
